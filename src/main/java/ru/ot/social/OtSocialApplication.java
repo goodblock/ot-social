040c6928e8605af7ae405848c2768121aecc0c14
user <user@user.ru>
2024-09-02 22:14:15 +0300

    fc

---
+++ src/main/java/ru/ot/social/OtSocialApplication.java
@@ -0,0 +1,15 @@
+package ru.ot.social;
+
+import lombok.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtSocialApplication {
+
+  @Generated
+  public static void main(String[] args) {
+    SpringApplication.run(OtSocialApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/ot/social/OtSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ot.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtSocialApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
